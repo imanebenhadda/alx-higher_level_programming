feca4086565e32f387b5bcd782ff1a48f086f727
imanebenhadda <imanebenhaddapama@gmail.com>
2023-10-02 17:47:13 +0100

    str

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 number = 98
-numbers=int number
 print ( f"{numbers},Battery street")
---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+str = "Holberton School"
+print ("holberton School" +"Holberton School"+ "Holberton School")
