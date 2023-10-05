9faa7efb0237bd88609f4d65c2f83e78ce780d37
imanebenhadda <imanebenhaddapama@gmail.com>
2023-10-05 21:20:28 +0100

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    argc = len(sys.argv)
+    if argc - 1 == 0:
+        print("0 arguments.")
+    else:
+        if argc - 1 == 1:
+            print("1 argument:")
+        else:
+            print("{} arguments:".format(argc - 1))
+        for i in range(1, argc):
+            print("{}: {}".format(i, sys.argv[i]))
+        
