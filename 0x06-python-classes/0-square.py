1c3949b43197adb7dc81cda314468985b02917d1
imanebenhadda <imanebenhaddapama@gmail.com>
2023-10-24 19:26:38 +0100

    square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+""" Square class """
+
+
+class Square(object):
+    ''' An empty class that defines a square '''
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" Square class """
+
+
+class Square(object):
+    ''' square class with size attribute '''
+    def __init__(self, size):
+        self.__size = size
